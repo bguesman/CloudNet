ef8976b3bf51da8e39b696f5484577c3de7dece6
bguesman <james_guesman@alumni.brown.edu>
2021-04-06 19:47:29 -0700

    Autoencoder now uses a style-based synthesis network with progressive growth

--- autoencoder.py
+++ autoencoder.py
@@ -20,7 +20,8 @@ class AutoEncoder(tf.keras.Model):
         ######^^^ DO NOT CHANGE ^^^##################
 
         # Define the optimizer we want to use to train the model.
-        self.optimizer = tf.keras.optimizers.Adam(learning_rate=learning_rate)
+        self.learning_rate = learning_rate
+        self.optimizer = tf.keras.optimizers.Adam(learning_rate=self.learning_rate)
 
         # Initialization standard deviation for the convolutional layers.
         self.initialize_std_dev = 0.05
@@ -29,11 +30,11 @@ class AutoEncoder(tf.keras.Model):
         self.low_res = 16
         
         # Dimension of encoded latent space
-        self.latent_dimension = 2048
+        self.latent_dimension = 512
 
         # Downsample layers. The goal here is to squish our image down to a latent code.
         self.encode_resolutions = [128, 64, 32, 16, 8, 4]
-        self.encode_filters = [16, 16, 32, 32, 64, 128]
+        self.encode_filters = [16, 32, 64, 128, 256, 512]
         self.encode_convs = [tf.keras.layers.Conv2D(filters=filters, kernel_size=5, 
             strides=1, padding='same', 
             kernel_initializer=tf.keras.initializers.TruncatedNormal(0, self.initialize_std_dev), 
@@ -53,27 +54,37 @@ class AutoEncoder(tf.keras.Model):
         self.encode_d1 = tf.keras.layers.Dense(self.latent_dimension)
 
         # Upsample layers. The goal here is to expand our latent code back out to a full image.
-        self.decode_resolutions = [8, 16, 32, 64, 128, 128, 256, 256]
-        self.decode_filters = [128, 64, 32, 32, 16, 16, 16, 16]
-        self.decode_lr = [tf.keras.layers.LeakyReLU() for i in range(len(self.decode_resolutions))]
-        self.decode_b = [tf.keras.layers.BatchNormalization() for i in range(len(self.decode_resolutions))]
-        self.decode_convs = [tf.keras.layers.Conv2D(filters=filters, kernel_size=3, 
+        self.current_resolution_index = 0
+        self.decode_resolutions = [32, 64]
+        self.decode_filters = [512, 256]
+        self.decode_layers = [2, 2]
+
+        self.decode_lr = [[tf.keras.layers.LeakyReLU() for _ in self.decode_layers] for _ in self.decode_resolutions]
+        self.decode_convs = [[tf.keras.layers.Conv2D(filters=filters, kernel_size=3, 
+                    strides=1, padding='same', 
+                    kernel_initializer=tf.keras.initializers.TruncatedNormal(0, self.initialize_std_dev), 
+                    bias_initializer=tf.keras.initializers.TruncatedNormal(0, self.initialize_std_dev)) 
+                for _ in self.decode_layers] 
+            for filters in self.decode_filters]
+        
+        # Style injection from latent code using adaptive instance normalization 
+        # (https://arxiv.org/pdf/1703.06868.pdf).
+        self.style_in = [[tfa.layers.InstanceNormalization(axis=3, center=False, scale=False) for _ in self.decode_layers] for _ in self.decode_resolutions]
+        # Learned mappings from style to feature standard deviation and mean.
+        self.style_std_dev_map = [[tf.keras.layers.Dense(filters) for _ in self.decode_layers] for filters in self.decode_filters]
+        self.style_mean_map = [[tf.keras.layers.Dense(filters) for _ in self.decode_layers] for filters in self.decode_filters]
+
+        # Output layers for each resolution.
+        self.output_conv = [tf.keras.layers.Conv2D(filters=1, kernel_size=3, 
             strides=1, padding='same', 
             kernel_initializer=tf.keras.initializers.TruncatedNormal(0, self.initialize_std_dev), 
-            bias_initializer=tf.keras.initializers.TruncatedNormal(0, self.initialize_std_dev)) for filters in self.decode_filters]
-        # Final layer.
-        self.final_conv = tf.keras.layers.Conv2D(filters=1, kernel_size=3, 
-            strides=1, padding='same', 
-            kernel_initializer=tf.keras.initializers.TruncatedNormal(0, self.initialize_std_dev), 
-            bias_initializer=tf.keras.initializers.TruncatedNormal(0, self.initialize_std_dev))
+            bias_initializer=tf.keras.initializers.TruncatedNormal(0, self.initialize_std_dev)) for _ in self.decode_resolutions]
 
 
-    @tf.function
     def call(self, input):
         encoded = self.encode(input)
         return self.decode(encoded, tf.image.resize(input, (self.low_res, self.low_res)))
     
-    @tf.function
     def encode(self, input):
         # Squish the input image down to the latent state.
         encoded = input
@@ -83,19 +94,47 @@ class AutoEncoder(tf.keras.Model):
             encoded = self.encode_max_pool[i](encoded)
         encoded = self.encode_flatten(encoded)
         encoded = self.encode_d0_lr(self.encode_d0_b(self.encode_d0(encoded)))
-        return self.encode_d1(encoded)
+        return tf.linalg.normalize(self.encode_d1(encoded), axis=1)[0]
 
     def decode(self, encoded, low_res):
         # Expand the low res image back into a high res image.
-        decoded = tf.reshape(encoded, [encoded.shape[0], 4, 4, int(self.latent_dimension/(4 * 4))]) #low_res
-        for i, res in enumerate(self.decode_resolutions):
-            decoded = self.decode_convs[i](decoded)
-            decoded = self.decode_lr[i](self.decode_b[i](decoded)) # TODO: might remove batch norm
-            decoded = tf.image.resize(decoded, (res, res))
-        return self.final_conv(decoded)
+        decoded = low_res
+        for i in range(self.current_resolution_index + 1):
+            res = self.decode_resolutions[i]
+            for j in range(self.decode_layers[i]):
+                decoded = self.decode_convs[i][j](decoded)
+                decoded = self.ada_in(self.decode_lr[i][j](decoded), encoded, i, j)
+                decoded = tf.image.resize(decoded, (res, res))
+        # Use the output layer for the current resolution we're training at.
+        return self.output_conv[self.current_resolution_index](decoded)
+
+    def ada_in(self, images, style, i, j):
+        # Normalize with instance normalization layer.
+        normalized = self.style_in[i][j](images)
+        mapped_std_dev = self.style_std_dev_map[i][j](style)
+        mapped_mean = self.style_mean_map[i][j](style)
+        # Expand across spatial resolution for broadcasting.
+        mapped_std_dev = tf.expand_dims(tf.expand_dims(mapped_std_dev, axis=1), axis=1)
+        mapped_mean = tf.expand_dims(tf.expand_dims(mapped_mean, axis=1), axis=1)
+        return mapped_std_dev * normalized + mapped_mean
+
+    def advance_resolution(self):
+        self.current_resolution_index = min(self.current_resolution_index + 1, len(self.decode_resolutions) - 1)
+        print("")
+        print("*****************************************************")
+        print("*****************************************************")
+        print("************ ADVANCING RESOLUTION TO " + str(self.decode_resolutions[self.current_resolution_index]) + " *************")
+        print("*****************************************************")
+        print("*****************************************************")
+        print("")
+        # Reset the optimizer
+        self.optimizer = tf.keras.optimizers.Adam(learning_rate=self.learning_rate)
+        # Make sure we only train the right output layer for this resolution
+        for i, layer in enumerate(self.output_conv):
+            layer.trainable = (i == self.current_resolution_index)
 
     def loss(self, prediction, ground_truth):
         # The loss is just the L2 norm between the prediction and the ground truth.
         # Basically, just the squared difference between the two images.
-        diff = prediction - ground_truth
+        diff = prediction - tf.image.resize(ground_truth, (prediction.shape[1], prediction.shape[2]))
         return tf.reduce_sum(diff * diff)
--- model_runner.py
+++ model_runner.py
@@ -14,47 +14,63 @@ import autoencoder
 def setup_model():
     return autoencoder.AutoEncoder()
 
-def train_batch(autoencoder, batch, epoch, batch_index):
+def print_avg_loss(avg_loss, epoch):
+    print("AVERAGE LOSS FOR EPOCH " + str(epoch) + ": " + str(avg_loss))
+
+def train_batch(autoencoder, batch):
     with tf.GradientTape() as tape:
         # Make a prediction.
         prediction = autoencoder(batch)
         # Compute the loss, which is just the difference between the
         # prediction and the original image.
         loss = autoencoder.loss(prediction, batch)
-        if (epoch % 1 == 0 and batch_index % 200 == 0):
-            print("LOSS, epoch " + str(epoch) + " batch " + str(batch_index) + ": " + str(loss))
-        
-    # Apply the gradients.
+    # Apply the gradients to the model variables.
     gradients = tape.gradient(loss, autoencoder.trainable_variables)
     autoencoder.optimizer.apply_gradients(zip(gradients, autoencoder.trainable_variables))
-
+    return loss
 
 def train_epoch(autoencoder, images, epoch, batch_size=8):
     # Shuffle
     np.random.shuffle(images)
     tensor_images = tf.convert_to_tensor(images, dtype=tf.float32)
-    # tensor_images = tfa.image.rotate(tensor_images, 2 * math.pi * tf.cast(tf.random.uniform([tensor_images.shape[0]], maxval=4, dtype=tf.int32), dtype="float"), fill_mode="WRAP")
-
     # Split into batches.
+    avg_loss = 0
     num_batches = int(images.shape[0] / batch_size)
     for i in range(num_batches):
-        train_batch(autoencoder,
-            tensor_images[i * batch_size:(i + 1) * batch_size, :, :, :], epoch, i)  
+        avg_loss += train_batch(autoencoder, tensor_images[i * batch_size:(i + 1) * batch_size, :, :, :])  
+    return avg_loss / num_batches
 
-def train(autoencoder, images, out_path, epochs=1):
+def train(autoencoder, images, out_path, epochs=1, resolution_advance_cutoff=0.001):
     # Grab the test image before we shuffle
-    test_image = images[0:1,:,:,:]
-    for i in range(epochs):
-        train_epoch(autoencoder, images, i)
-        if i % 1 == 0:
-            test(autoencoder, test_image, out_path + "/test_" + str(i))
-        # TODO: checkpoint
+    test_image = np.copy(images[0:1,:,:,:])
+
+    # Create a list of average losses to use indeciding when to advance resolution
+    average_loss_diffs = [-4, -3, -2, -1]
+    avg_loss = 10e32
+
+    for i in range(0, epochs):
+        prev_avg_loss = avg_loss
+
+        avg_loss = train_epoch(autoencoder, images, i)
+        print_avg_loss(avg_loss, i)
+        
+        test(autoencoder, test_image, out_path + "/test_" + str(i))
+        
+        average_loss_diffs.pop(0)
+        average_loss_diffs.append(avg_loss - prev_avg_loss)
+        average_diff = sum(average_loss_diffs) / len(average_loss_diffs)
+        print("Average loss diff: " + str(average_diff))
+        if average_diff > resolution_advance_cutoff:
+            autoencoder.advance_resolution()
+            average_loss_diffs = [-4, -3, -2, -1]
+            avg_loss = 10e32
 
 def test(autoencoder, image, path):
-    # Generate a random image
     generated = autoencoder(image)
+    low_res = tf.image.resize(image, (autoencoder.low_res, autoencoder.low_res))
+    cv2.imwrite(path + "_low_res.png", 255 * np.clip(tf.squeeze(low_res[0,:,:,0]).numpy(), 0, 1))
+    cv2.imwrite(path + "_upscaled.png", 255 * np.clip(tf.squeeze(generated[0,:,:,0]).numpy(), 0, 1))
     cv2.imwrite(path + "_real.png", 255 * np.clip(tf.squeeze(image[0,:,:,0]).numpy(), 0, 1))
-    cv2.imwrite(path + "_generated.png", 255 * np.clip(tf.squeeze(generated[0,:,:,0]).numpy(), 0, 1))
 
 def load_images(path):
     images = []
